17ddf091bce47862909701f8a11d3cab3aa9c09d
Sagar Mangulkar <smangul1@bl-cons.pods.bu.int>
2017-02-18 16:09:37 -0500

    Implement move right left control

--- DoodleJump/PlayViewController.m
+++ DoodleJump/PlayViewController.m
@@ -17,14 +17,24 @@
                                    selector:@selector(jump:)
                                    userInfo:nil
                                     repeats:YES];
+    [NSTimer scheduledTimerWithTimeInterval:0.1
+                                     target:self
+                                   selector:@selector(moveLeftRight:)
+                                   userInfo:nil
+                                    repeats:YES];
 }
 
 -(void)jump:timer{
     //simple jump
     [UIView animateWithDuration:1.0 animations:^{
         CGRect imageFrame = self.imageDoodler.frame;
-        imageFrame.origin.y -= 200;
- //       imageFrame.origin.x = _slider.value * 100;
+        if (imageFrame.origin.y > 0) {
+            imageFrame.origin.y -= 200;
+        }
+        if (imageFrame.origin.x >= 0 && imageFrame.origin.x <= 327) {
+            imageFrame.origin.x = _slider.value * 327;
+        }
+   //     NSLog(@"Slider value: @%f, Doodler value: @%f", _slider.value, _slider.value * 327);
         self.imageDoodler.frame = imageFrame;
     }
                      completion:^(BOOL finished){
@@ -35,9 +45,17 @@
                          }];
                      }
      ];
-    
-    //moving left-right
-    NSLog(@"Slider value: @%f", _slider.value);
+}
+
+-(void)moveLeftRight:timer{
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect imageFrame = self.imageDoodler.frame;
+        if (imageFrame.origin.x >= 0 && imageFrame.origin.x <= 327) {
+            imageFrame.origin.x = _slider.value * 327;
+        }
+//        NSLog(@"MoveRightLeft- Slider value: @%f, Doodler value: @%f", _slider.value, _slider.value * 327);
+        self.imageDoodler.frame = imageFrame;
+    }];
 }
 
 - (IBAction)buttonTest:(id)sender {
